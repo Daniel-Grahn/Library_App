021b3f57762ab257966538bc81626444863294c9
Daniel <dangra0530@skola.goteborg.se>
2026-01-02 00:13:42 +0100

    Configurate Selenium

---
+++ integrations/src/test/java/se/yrgo/integrations/GeneralStepDefinitions.java
@@ -0,0 +1,49 @@
+package se.yrgo.integrations;
+//I do not know why the package lock like this!
+import static org.junit.jupiter.api.Assertions.*;
+
+import io.cucumber.java.*;
+import io.cucumber.java.en.*;
+
+import java.net.MalformedURLException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+
+public class GeneralStepDefinitions {
+    private static WebDriver driver;
+
+    @Before
+    public void setupWebDriver() {
+        try {
+            ChromeOptions options = new ChromeOptions();
+            driver = new RemoteWebDriver(new URL("http://localhost:4444"),
+                    options, false);
+        } catch (MalformedURLException e) {
+            fail(e);
+        }
+    }
+
+    @After
+    public void shutdownWebDriver() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+
+    @Given("the user is on the start page.")
+    public void the_user_is_on_the_start_page() {
+        driver.get("http://frontend");
+        if (!"The Library".equals(driver.getTitle())) {
+            throw new IllegalStateException("Not on the start page");
+        }
+    }
+}
+
